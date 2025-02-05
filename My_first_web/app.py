fa11f8b1243826b40f72833bc96be9e2b2dc9944
Jakub Kiedroň <allien56@seznam.cz>
2025-02-05 20:29:59 +0100

    2

--- My_first_web/app.py
+++ My_first_web/app.py
@@ -5,15 +5,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("/pages/home.html")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("/pages/about.html")
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("/pages/contact.html")
+
+
+@app.route("/calc/<int:a>/<int:b>")
+def calculator(a, b):
+    #a = 10
+    #b = 20
+    return render_template("/pages/calc.html", a =a, b=b, suma=a+b)
+#**kwargs
 
 if __name__ == "__main__":
     app.run(debug=True)
